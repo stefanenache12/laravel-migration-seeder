bebe04bced0d5bc9210535b34ed0f466dc6af3c9
stefanenache12 <stefanenache12@gmail.com>
2023-09-12 16:29:43 +0200

    ADD SEEDER

---
+++ database/seeders/TrainsTableSeeder.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\Train;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class TrainsTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        for ($i=0; $i < 15; $i++) { 
+
+            $train = new Train();
+
+            $train->train_company = fake()->word();
+            $train->departure_station = fake()->word();
+            $train->arrival_station = fake()->word();
+            $train->departure_time = fake()->dateTimeBetween('-3 day');
+            $train->arrival_time = fake()->dateTimeBetween('-2 day');
+            $train->train_number = fake()->randomNumber(5, true);
+            $train->wagons = fake()->numberBetween(9, 20);
+            $train->in_time = fake()->numberBetween(0 , 1);
+            $train->deleted = 0;
+            $train->save();
+        }
+    }
+}
